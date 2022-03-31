361db85f985afff55ac5225660d3f6eb47100616
nirzaf <mfmfazrin1986@gmail.com>
2022-03-31 13:13:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Create a class of vehicle 
+
+Vehicle v1 = new Vehicle();
---
+++ Vehicle.cs
@@ -0,0 +1,5 @@
+﻿public class Vehicle
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
